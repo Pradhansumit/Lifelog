6e80ee26fb3f24d05d6b6a8e8a826eda29b22767
Pradhansumit <pradhansumit026@gmail.com>
2025-06-23 20:28:49 +0530

    Added modal view to the calendar.

--- frontend/src/components/Main/CalendarView.tsx
+++ frontend/src/components/Main/CalendarView.tsx
@@ -17,6 +17,7 @@ const moodColors = {
 const CalendarView = () => {
   const [events, setEvents] = useState([]);
   const [selectedEntry, setSelectedEntry] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const apiEntries = async () => {
     try {
@@ -55,6 +56,7 @@ const CalendarView = () => {
         events={events}
         eventClick={(info) => {
           const { title, startStr, extendedProps } = info.event;
+          setIsOpen(true);
           setSelectedEntry({
             mood: title,
             note: extendedProps.note,
@@ -76,6 +78,71 @@ const CalendarView = () => {
             <span className="font-medium">Note:</span>{" "}
             {selectedEntry.note || "No note"}
           </p>
+        </div>
+      )}
+
+      {/* modal */}
+      {isOpen && (
+        <div>
+          <div
+            className="relative z-10"
+            aria-labelledby="dialog-title"
+            role="dialog"
+            aria-modal="true"
+          >
+            <div
+              className="fixed inset-0 bg-gray-500/75 transition-opacity"
+              aria-hidden="true"
+            ></div>
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3
+                          className="text-2xl mb-4 font-semibold text-gray-900"
+                          id="dialog-title"
+                        >
+                          {selectedEntry.date}
+                        </h3>
+                        <div className="mt-2">
+                          <p>
+                            <span className="font-medium  tracking-wide">
+                              Mood:
+                            </span>{" "}
+                            <span className="capitalize">
+                              {selectedEntry.mood}
+                            </span>
+                          </p>
+                          <p>
+                            <span className="font-medium tracking-wide">
+                              Note:
+                            </span>{" "}
+                            <span className="capitalize">
+                              {selectedEntry.note || "No note"}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto cursor-pointer"
+                      onClick={() => {
+                        setIsOpen(false);
+                      }}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
